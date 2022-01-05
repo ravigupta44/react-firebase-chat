de889f782a1a6063265edc2993b250ef2a378191
ravigupta44 <raviguptaamba4@gmail.com>
2022-01-05 19:51:49 +0530

    blank profile pic added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Camera from '../components/svg/Camera';
-import Img from "../profilepic.jpeg";
+import Img from "../blankpp.png";
 import {storage, db, auth} from "../firebase";
 import {ref, getDownloadURL, uploadBytes, deleteObject} from "firebase/storage";
 import {getDoc, doc, updateDoc} from "firebase/firestore";
